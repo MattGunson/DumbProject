2559bc144936c855a17f7dd1f81ed62d22ec0015
Matthew Gunson <mgunson@qualtrics.com>
2023-02-24 21:44:06 -0700

    Remove whitespace

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -40,7 +40,6 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        
         int o = (int) Math.round((Math.pow(1.618033988749895, n) - Math.pow(-0.6180339887498949, n)) / 2.23606797749979);        
         return o;
     }
